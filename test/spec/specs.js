fcd152f3f4942fef933efdc1cec865864fcca327
Shyam Seshadri <shyamseshadri@gmail.com>
2013-11-10 20:59:05 +0100

    Session 4 - Step 2: Test for landing controller

--- test/spec/specs.js
+++ test/spec/specs.js
@@ -18,3 +18,30 @@ describe('Service: AlertService', function () {
     expect(alertService.get()).toBeNull();
   });
 });
+
+describe('Controller: LandingCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('stockMarketApp'));
+
+  var ctrl, scope, mockBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $httpBackend) {
+    scope = {};
+    mockBackend = $httpBackend;
+    mockBackend.expectGET('/api/stocks').respond([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
+    ctrl = $controller('LandingCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should fetch all stocks on load', function () {
+    expect(ctrl.stocks).toEqual([]);
+
+    mockBackend.flush();
+
+    expect(ctrl.stocks).toEqual([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
+  });
+});
+
