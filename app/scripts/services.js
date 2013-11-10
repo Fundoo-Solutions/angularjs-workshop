8c9f27ca6bb3301d27928f3d84aabde71cd52cdf
Shyam Seshadri <shyamseshadri@gmail.com>
2013-11-10 08:31:14 +0530

    Session 2 - Step 5: Add login checks and resolve functionality

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -25,16 +25,39 @@ angular.module('stockMarketApp')
         return $http.get('/api/stocks/' + code);
       }
     };
-  }]).factory('UserService', ['$http', function($http) {
+  }]).factory('UserService', ['$http', '$q', function($http, $q) {
+    var user = {};
+    var loggedIn = false;
+    var loginSuccess = function(resp) {
+      user = resp.data.user;
+      loggedIn = true;
+      return user;
+    };
+    var loginFailure = function(err) {
+      loggedIn = false;
+      console.log('Rejecting');
+      return $q.reject(err.data);
+    };
+
     return  {
+      isLoggedIn: function() {
+        return loggedIn;
+      },
       login: function(username, pwd) {
-        return $http.post('/api/login', {username: username, password: pwd});
+        return $http.post('/api/login', {username: username, password: pwd}).then(loginSuccess, loginFailure);
       },
       logout: function() {
         return $http.post('/api/logout', {});
       },
       register: function(username, pwd) {
-        return $http.post('/api/register', {username: username, password: pwd});
+        return $http.post('/api/register', {username: username, password: pwd}).then(loginSuccess, loginFailure);
+      },
+      tokens: function() {
+        if (loggedIn) {
+          return $q.when(user);
+        } else {
+          return $http.post('/api/token', {}).then(loginSuccess, loginFailure);
+        }
       }
     };
   }]);
