d801382eb32dd83136af826d5763382eb2e9f14a
Shyam Seshadri <shyamseshadri@gmail.com>
2013-11-10 06:41:53 +0530

    Initial commit - Skeleton App

---
+++ test/spec/specs.js
@@ -0,0 +1,61 @@
+'use strict';
+
+describe('Controller: LandingCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('stockMarketApp'));
+
+  var ctrl, scope, mockBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $httpBackend) {
+    scope = {};
+    mockBackend = $httpBackend;
+    mockBackend.expectGET('/api/stocks').respond([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
+    ctrl = $controller('LandingCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should fetch all stocks on load', function () {
+    expect(ctrl.stocks).toEqual([]);
+
+    mockBackend.flush();
+
+    expect(ctrl.stocks).toEqual([{ticker: 'GOOG'}, {ticker: 'AMZN'}]);
+  });
+});
+
+describe('Controller: AuthCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('stockMarketApp'));
+
+  var ctrl, scope, mockBackend, $loc, userService;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $httpBackend, $location, UserService) {
+    scope = {};
+    mockBackend = $httpBackend;
+    userService = UserService;
+    $loc = $location;
+
+    ctrl = $controller('AuthCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should register the user', function () {
+    ctrl.username = 'shyam';
+    ctrl.password = 'test';
+
+    mockBackend.expectPOST('/api/register', {username: 'shyam', password: 'test'}).respond({user: {name: 'shyam'}});
+
+    expect(userService.isLoggedIn()).toBeFalsy();
+    ctrl.register();
+
+    mockBackend.flush();
+    expect($loc.path()).toEqual('/mine');
+    expect(userService.isLoggedIn()).toBeTruthy();
+  });
+});
