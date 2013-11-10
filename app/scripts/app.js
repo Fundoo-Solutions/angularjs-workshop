90f7168cce70c4ace65d042c3ea5feb37178de4e
Shyam Seshadri <shyamseshadri@gmail.com>
2013-11-10 08:21:25 +0530

    Session 2 - Step 4: Add routes for register, login, list and all

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,28 @@
-angular.module('stockMarketApp', []);
+'use strict';
+
+angular.module('stockMarketApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/list.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl',
+        controllerAs: 'loginCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/signup.html',
+        controller: 'AuthCtrl',
+        controllerAs: 'signupCtrl'
+      })
+      .when('/mine', {
+        templateUrl: 'views/mine.html',
+        controller: 'MyStocksCtrl',
+        controllerAs: 'myStocksCtrl'
+      })
+      .otherwise({
+        redirectTo: '/all'
+      });
+  });
