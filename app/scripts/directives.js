893d0dfea5e3ac1da9199de1d05c478ddfdaeb10
Shyam Seshadri <shyamseshadri@gmail.com>
2013-11-10 19:21:39 +0100

    Session 3 - Step 3: Fully contained directive with data passed in

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,6 +1,18 @@
 angular.module('stockMarketApp').directive('stockDash', [function() {
   return {
     restrict: 'A',
-    templateUrl: 'views/stock-dash.html'
+    templateUrl: 'views/stock-dash.html',
+    scope: {
+      stockData: '='
+    },
+    link: function($scope, $element, $attrs) {
+
+      $scope.getChange = function() {
+        return Math.ceil((($scope.stockData.price - $scope.stockData.previous) / $scope.stockData.previous) * 100);
+      };
+      $scope.getChangeClass = function() {
+        return $scope.getChange() >= 0 ? 'positive' : 'negative';
+      };
+    }
   };
 }]);
